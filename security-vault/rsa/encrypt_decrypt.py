542424764faf5044fce4080c10b07b333fb1e862
al3phnvll <alephnvll@gmail.com>
2026-09-24 14:28:50 +0200

    day-2 encrypt_decrypt

---
+++ security-vault/rsa/encrypt_decrypt.py
@@ -0,0 +1,44 @@
+from keygen import RSAKeyPair, keygen
+
+def mod(m,e,n) :
+    res_mod = 1
+    base = m % n
+    pow_e = []
+    while e > 0:
+        pow_e.append(e % 2)
+        e = e // 2
+
+    for i in range(len(pow_e)):
+        if(pow_e[i] == 1):
+            res_mod = (res_mod * base) % n
+        base = (base * base) % n
+        
+    return res_mod
+
+def encrypt(m, e, n):
+    return mod(m, e, n)
+
+def decrypt(c, d, n):
+    return mod(c, d, n)
+
+def text_to_int(text):
+    text = text.encode()
+    return int.from_bytes(text)
+
+def int_to_text(i):
+    length_of_bits = ((i.bit_length() + 7) // 8) 
+    return int.to_bytes(i ,length_of_bits).decode()
+
+kp = keygen()
+
+message = "hi"
+
+m = text_to_int(message)
+print(m)
+cipher = encrypt(m,kp.e, kp.n)
+print(cipher)
+
+decoded = decrypt(cipher,kp.d, kp.n)
+decode_cipher = int_to_text(decoded)
+
+print(decode_cipher)
